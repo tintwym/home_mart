d8e015f5906fc201882b33664fbec728cf5f8421
TintWaiYanMin <tintwaiyanmin.sg@gmail.com>
2026-03-29 21:25:42 +0800

    Update Logo

--- config/view.php
+++ config/view.php
@@ -26,11 +26,17 @@ return [
     |
     */
 
-    'compiled' => env(
-        'VIEW_COMPILED_PATH',
-        env('VERCEL')
-            ? rtrim(sys_get_temp_dir(), DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.'laravel-view-compiled'
-            : storage_path('framework/views')
-    ),
+    'compiled' => env('VIEW_COMPILED_PATH', (function (): string {
+        $tmpCompiled = rtrim(sys_get_temp_dir(), DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.'laravel-view-compiled';
+
+        // Vercel / serverless: only /tmp is writable; VERCEL is usually set but not always visible to PHP.
+        if (filter_var(env('VERCEL'), FILTER_VALIDATE_BOOLEAN)) {
+            return $tmpCompiled;
+        }
+
+        $storageViews = storage_path('framework/views');
+
+        return is_writable(dirname($storageViews)) ? $storageViews : $tmpCompiled;
+    })()),
 
 ];
